c7affd1b25d80a3a73cc6895230342ee7e0bc9e7
Rifala <arifatulfadia.1620@gmail.com>
2022-06-12 13:21:14 +0700

    Stu dying

---
+++ JsonAPI/src/Sorting/InsertionSort.java
@@ -0,0 +1,66 @@
+package Model;
+
+public class InsertionSort {
+    private int[] data;
+    private String[] dataNama;
+
+    public void setDataNama(String[] dataNama) {this.dataNama = dataNama;}
+
+    public void setData(int[] data){this.data = data;}
+
+
+
+    public void InsertionSortAngka(){
+        int size = this.data.length;
+
+        for (int step = 1; step < size; step++){
+            int key = this.data[step];
+            int j = step - 1;
+
+            while (j >= 0 && key < this.data[j]){
+                this.data[j + 1] = this.data[j];
+                --j;
+            }
+
+            this.data[j + 1] = key;
+        }
+    }
+
+    public void InsertionSortHrf(){
+        int size = this.dataNama.length;
+
+        for (int step = 1; step < size; step++){
+            String key = this.dataNama[step];
+            int j = step - 1;
+
+            while (j >= 0 && (key.compareTo(this.dataNama[j]) < 0)){
+                this.dataNama[j + 1] = this.dataNama[j];
+                --j;
+            }
+
+            this.dataNama[j + 1] = key;
+        }
+    }
+
+
+    public void PrintSort(){
+        for (int datum : this.data){
+            System.out.println(datum + " ");
+        }
+
+        for (String datum : this.dataNama){
+            System.out.println(datum + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] nama = {"Windy","Budi","Susi","Agus","Beni"};
+        int[] age = {12,7,2,1,0,67,78,95};
+        InsertionSort s = new InsertionSort();
+        s.setData(age);
+        s.setDataNama(nama);
+        s.InsertionSortAngka();
+        s.InsertionSortHrf();
+        s.PrintSort();
+    }
+}
